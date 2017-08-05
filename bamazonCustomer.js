81bf85ab9da78cae3e1ff14afe9b95f782faac9b
Joaquin Ignacio <Keen@Revolt.local>
2017-08-04 23:22:53 -0700

    updated files near completion

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,6 +13,9 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+// global variable to hold the number of items for sale. 
+var totalItems;
+
 //creates a connection to MySQL database
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -35,11 +38,15 @@ function itemList() {
 	var query = "SELECT * FROM products";
 	connection.query(query, function(err, res){
 
+	totalItems = res.length;
+
 	for (i=0; i<res.length; i++) {
 		console.log("==============", "\nItem #: "+ res[i].item_id + "\nItem: "+ res[i].product_name + "\nDepartment: " +res[i].department_name + "\nPrice: $" + res[i].price + "\nQuantity in Stock: " + res[i].stock_quantity);
+		availableItems.push(res[i]);
 		}
+		// console.log(availableItems);
+		options();
 	});
-	options();
 };
 
 // provides two options to buy an item# and how many of them.
@@ -48,8 +55,9 @@ function options(){
 		name: "id",
 		type: "input",
 		message: "Which item would you like to buy? (List the item number)",
+		//validation that the number input is a number and less than or equal to length of array. 
 		validate: function(value) {
-			if (isNaN(value) === false && value <=10) {
+			if (isNaN(value) === false && value <=totalItems) {
 				return true;
 			} else {
 				return false;
@@ -69,12 +77,56 @@ function options(){
 			}
 		}
 	]).then(function(answer){
-		console.log(answer);
-	})
+		// console.log(answer);
+
+		// check for if items being sold are in stock; if not, then send back to beginning. 
+		var query = "SELECT item_id, product_name, stock_quantity, price FROM products WHERE ?"
+		connection.query(query, {item_id: answer.id}, function (err, res) {
+			// console.log(res[0].stock_quantity);
+
+			var aquiredItem = res[0].product_name;
+
+			if (answer.quantity <= res[0].stock_quantity) {
+				// send query to update the mysql and display that the items were purchased. 
+				
+				var newStock = res[0].stock_quantity - answer.quantity;
+				var purchaseCost = res[0].price*answer.quantity;
+				
+				var query2  = " UPDATE products SET ? WHERE ?";
+				connection.query(query2, [{ stock_quantity: newStock }, { item_id: answer.id }], function (err, res){
+					
+					if (err) throw err;
+					console.log("SUCCESS! You have purchased "+ answer.quantity + " of item: " + aquiredItem+ "!" + "\nFor a total cost of: $"+ purchaseCost);
+
+					maybeProceed();
+				});
+
+			} else {
+				console.log("\nSorry, not enough in stock!")
+				options();
+			}
+		});
+
+
+
+	});
 };
 
-
-
+function maybeProceed () {
+	inquirer.prompt([{
+		type: 'list',
+		name: 'proceed',
+		message: 'Would you like to make another purchase?',
+		choices: ['Yes', 'No']
+	}]).then(function(ans){
+		// console.log(ans);
+		if (ans.proceed === "Yes") {
+			itemList();
+		} else {
+			console.log("THANK YOU FOR SHOPPING WITH US!");
+		}
+	})
+}
 
 
 
